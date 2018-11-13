50c21f21b9ffcb8af8ca72cecb0ec8dc85fda671
Edunno <tamericandream@hotmail.com>
2018-11-13 09:45:45 +0100

    LogCalculator

---
+++ ExamProject2/src/main/java/calculators/LogCalculator.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculators;
+
+/**
+ *
+ * @author Esben
+ */
+public class LogCalculator {
+ 
+    public int mainCalc(double a, double b){
+        int logs = ((int) (a / 2.5)+1) * ((int) (b/2.5)+1);
+        return logs;
+    }
+}
