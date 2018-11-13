3c41dc971b000d532edd59d46d907a0e64d18af1
GodlikeCPU <dan.tomicic@gmail.com>
2018-11-13 13:01:27 +0100

    added SpecialRoofCalculator - mostly a test to see if pushing works

--- ExamProject2/src/main/java/calculators/SpecialRoofCalculator.java
+++ ExamProject2/src/main/java/calculators/SpecialRoofCalculator.java
@@ -10,12 +10,53 @@ package calculators;
  * @author Dan
  */
 public class SpecialRoofCalculator {
+
+    int widthOfRoof;
+    int rafterLenght;
+    int slopeOfRoof;
+    int midRoofDegrees;
     
-int width;
-int 
-int slope;
-int 
     
+    public SpecialRoofCalculator(int widthOfRoof, int slopeOfRoof){
+        this.slopeOfRoof = slopeOfRoof;
+        this.widthOfRoof = widthOfRoof;
+    }
+
+    public static void main(String[] args) {
+        SpecialRoofCalculator src = new SpecialRoofCalculator(360, 20);
+        src.calcMidRoofDegrees();
+        src.calcRafterLength();
+        
+    }
+
+    /**
+     * This method calculates the middle angle og the roof, given the 2 other angles.
+     * 
+     * We know that a triangle is 180 degrees in total, and that the two bottom angles are always equal.
+     * 
+     * 
+     * @return 
+     */
     
+    int calcMidRoofDegrees() {
+        this.midRoofDegrees = 180 - (slopeOfRoof * 2);
+        return midRoofDegrees;
+    }
     
+    /**
+     * This method calculates the side of the roof lenght using sinus relations.
+     * 
+     * 
+     * @return 
+     */
+
+    double calcRafterLength() {
+        double AA = Math.toRadians(slopeOfRoof);
+        double BB = Math.toRadians(midRoofDegrees);
+        double bb = widthOfRoof;
+        double aa = (Math.sin(AA) * bb) / (Math.sin(BB));
+        this.rafterLenght = (int) aa;
+        return aa;
+    }
+
 }
