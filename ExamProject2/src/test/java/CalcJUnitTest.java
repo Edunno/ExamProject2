81ac584f95b74990231d1bb77e0011b04558200f
Edunno <tamericandream@hotmail.com>
2018-11-13 13:11:47 +0100

    Test for Roof, Band and Logs

--- ExamProject2/src/test/java/CalcJUnitTest.java
+++ ExamProject2/src/test/java/CalcJUnitTest.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 
-
+import calculators.BandCalculator;
 import calculators.LogCalculator;
 import calculators.RoofCalculator;
 import org.junit.Test;
@@ -15,33 +15,42 @@ import static org.junit.Assert.*;
  * @author Esben
  */
 public class CalcJUnitTest {
-    
 
     // TODO add test methods here.
     // The methods must be annotated with annotation @Test. For example:
     //
     // @Test
     // public void hello() {}
-    
     @Test
-    public void testLogs(){
+    public void testLogs() {
         LogCalculator lc = new LogCalculator();
         double aLen = 7.8;
         double bLen = 3.7;
-        
+
         int r = lc.mainCalc(aLen, bLen);
-        
-        assertEquals(8,r);
+
+        assertEquals(8, r);
     }
-    
+
     @Test
-    public void testRoof(){
+    public void testRoof() {
         RoofCalculator rc = new RoofCalculator();
         double aLen = 7.8;
         double bLen = 3.7;
-        
+
         int r = rc.RoofCalc(aLen, bLen);
+
+        assertEquals(28, r);
+    }
+
+    @Test
+    public void testBand() {
+        BandCalculator bc = new BandCalculator();
+        double aLen = 7.8;
+        double bLen = 3.7;
         
-        assertEquals(28,r);
+        double r = bc.bandCalc(aLen, bLen);
+        
+        assertEquals(17.26,r,0.1);
     }
 }
