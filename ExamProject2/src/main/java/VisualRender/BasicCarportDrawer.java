e5acff4dde3426f6bc085bb2442c90b266adc214
Edunno <tamericandream@hotmail.com>
2018-11-21 09:43:26 +0100

    BasicCarportDrawer logDrawer start

--- ExamProject2/src/main/java/VisualRender/BasicCarportDrawer.java
+++ ExamProject2/src/main/java/VisualRender/BasicCarportDrawer.java
@@ -22,7 +22,7 @@ public class BasicCarportDrawer {
     RectangleDrawer rd = new RectangleDrawer();
 
     public static void main(String[] args) {
-        BasicCarportDrawer bc = new BasicCarportDrawer(5.5, 6.8);
+        BasicCarportDrawer bc = new BasicCarportDrawer(10.0, 6.8);
         System.out.println(bc.startDraw());
     }
 
@@ -30,15 +30,15 @@ public class BasicCarportDrawer {
         if (sizeY > sizeX) {
             this.sizeX = sizeY;
             this.sizeY = sizeX;
-            this.svgX =(int) (sizeY * 100);
-            this.svgY =(int) (sizeX * 100);
+            this.svgX = (int) (sizeY * 100);
+            this.svgY = (int) (sizeX * 100);
         } else {
             this.sizeX = sizeX;
             this.sizeY = sizeY;
-            this.svgX =(int) (sizeX * 100);
-            this.svgY =(int) (sizeY * 100);
+            this.svgX = (int) (sizeX * 100);
+            this.svgY = (int) (sizeY * 100);
         }
-        this.start = "<SVG width=\"" + 800 + "\" height=\"" + 600 + "\">";
+        this.start = "<SVG width=\"" + svgX * 1.1 + "\" height=\"" + svgY * 1.1 + "\">";
 
     }
 
@@ -49,17 +49,17 @@ public class BasicCarportDrawer {
     }
 
     public String startDraw() {
-        StropCalculator sc = new StropCalculator(sizeX, sizeY, 2.5); // TODO Spacing selecter should be implemented into this class.
-        int strops = sc.amount();
-        int stropLength = (int) (sc.length()*100);
-        start += stropDrawer(strops, stropLength);
         LogCalculator lc = new LogCalculator();
         int xSide = lc.getLogAmountsXSide(sizeX, sizeY);
         int allLogs = lc.mainCalc(sizeX, sizeY);
         start += logDrawer(xSide, allLogs);
+        StropCalculator sc = new StropCalculator(sizeX, sizeY, 2.5); // TODO Spacing selecter should be implemented into this class.
+        int strops = sc.amount();
+        int stropLength = (int) (sc.length() * 100);
+        start += stropDrawer(strops, stropLength);
         RafterCalculator rc = new RafterCalculator();
         int rafts = rc.RaftCalc(sizeX, sizeY);
-        int raftLength = (int) (rc.RaftLength(sizeX, sizeY)*100);
+        int raftLength = (int) (rc.RaftLength(sizeX, sizeY) * 100);
         start += raftDrawer(rafts, raftLength);
 
         start += "</SVG>";
@@ -70,10 +70,9 @@ public class BasicCarportDrawer {
         int logDim = 4;
         String res = "";
         for (int i = 0; i < rafts; i++) {
-            if(i>0){
-            res += rd.RectangleDrawer((svgX*i/(rafts-1))-logDim/2, 0, raftLength, logDim);
-            }
-            else{
+            if (i > 0) {
+                res += rd.RectangleDrawer((svgX * i / (rafts - 1)) - logDim / 2, 0, raftLength, logDim);
+            } else {
                 res += rd.RectangleDrawer(0, 0, raftLength, logDim);
             }
         }
@@ -81,20 +80,20 @@ public class BasicCarportDrawer {
     }
 
     private String logDrawer(int xSide, int allLogs) {
-        int logDimA = 8;
-        int logDimB = 8;
+        int logDimA = 12;
+        int logDimB = 12;
         String res = "";
-        int localSvgX = svgX-30;
-        int localSvgY = svgY-30;
-        int ySide = allLogs/xSide;
-        int startY = 15-(logDimB/2);
-        for(int i = 0; i < ySide;i++){
-            int startX = 15-(logDimA/2);
-            for(int j = 0; j < xSide;j++){
-                res +=rd.RectangleDrawer(startX, startY, logDimA, logDimB);
-                startX += (localSvgX/(xSide-1));
+        int localSvgX = svgX - 30;
+        int localSvgY = svgY - 30;
+        int ySide = allLogs / xSide;
+        int startY = 15 - (logDimB / 2);
+        for (int i = 0; i < ySide; i++) {
+            int startX = 15 - (logDimA / 2);
+            for (int j = 0; j < xSide; j++) {
+                res += rd.RectangleDrawer(startX, startY, logDimA, logDimB);
+                startX += (localSvgX / (xSide - 1));
             }
-            startY += (localSvgY/(ySide-1));
+            startY += (localSvgY / (ySide - 1));
         }
         return res;
     }
@@ -102,11 +101,11 @@ public class BasicCarportDrawer {
     private String stropDrawer(int strops, int stropLength) {
         int logDim = 6;
         String res = "";
-        int localSvgY = svgY-30;
-        int startY = 15-(logDim/2);
-        for(int i = 0; i < strops;i++){
+        int localSvgY = svgY - 30;
+        int startY = 15 - (logDim / 2);
+        for (int i = 0; i < strops; i++) {
             res += rd.RectangleDrawer(0, startY, logDim, stropLength);
-            startY += (localSvgY/(strops-1));
+            startY += (localSvgY / (strops - 1));
         }
         return res;
     }
