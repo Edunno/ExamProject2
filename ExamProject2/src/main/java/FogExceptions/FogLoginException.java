c77382e0dc26ddb586369a1af95f9834286ca12f
KimPPedersen <KimPPedersen@10.50.130.251>
2018-12-03 10:22:20 +0100

    Exception relocated, minor adjustments of code

--- ExamProject2/src/main/java/FogExceptions/FogLoginException.java
+++ ExamProject2/src/main/java/FogExceptions/FogLoginException.java
@@ -1,31 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package FogExceptions;
 
 /**
- * THIS IS INTENDED TO BE THE LOGIN EXCEPTION!
- * NEEDS TO BE DONE
- * @author KimPPedersen
+ * The purpose of FogLoginException is to...
+ * @author DECK-CS, Kasper
  */
 public class FogLoginException extends Exception {
 
-    /**
-     * Creates a new instance of <code>FogException</code> without detail
-     * message.
-     */
-    public FogLoginException() {
-    }
-
-    /**
-     * Constructs an instance of <code>FogException</code> with the specified
-     * detail message.
-     *
-     * @param msg the detail message.
-     */
     public FogLoginException(String msg) {
         super(msg);
     }
+    
+
 }
